8aee53f0b115fd14c92f5bc77acaf3592c3e2597
hamadali <ihamadfouad@icloud.com>
2025-07-06 18:03:05 +0300

    Move localization to lib directory

--- src/context/LocalizationContext.tsx
+++ src/context/LocalizationContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { localizationSets } from "@/app/localization";
+import { localizationSets } from "@/lib/localization";
 
 type Lang = 'en' | 'ar';
 
