4b8bafb4af2af7b91cc6ae51a460f8653f008212
hamadali <ihamadfouad@icloud.com>
2025-07-06 17:40:20 +0300

    Clean up middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 
-// 1. Specify protected and public routes
-const protectedRoutes = ["/console"];
+// Specify protected and public routes
+const authenticatedRoutes = ["/console"];
 const publicRoutes = ["/login", "/signup", "/"];
-const PUBLIC_FILE = /\.(.*)$/;
 
 export default async function middleware(req: NextRequest) {
-  console.log("🔍 Middleware is running...");
-  // 2. Check if the current route is protected or public
+
   const path = req.nextUrl.pathname;
-  const isProtectedRoute = protectedRoutes.includes(path);
+  const isProtectedRoute = authenticatedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
 
-  // 3. Decrypt the session from the cookie
+  // Get session token from the cookie
   const token = req.cookies.get("token")?.value
 
-  // 4. Redirect to /login if the user is not authenticated
+  // Redirect to /login if the user is not authenticated
   if (isProtectedRoute && !token) {
     return NextResponse.redirect(new URL("/authentication/login", req.nextUrl));
   }
 
-  // 5. Redirect to /console if the user is authenticated
+  // Redirect to /console if the user is authenticated
   if (
     isPublicRoute &&
     token &&
@@ -31,11 +29,6 @@ export default async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
-
-// Routes Middleware should not run on
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-// };
 
 export const config = {
   matcher: ["/:path*"],
