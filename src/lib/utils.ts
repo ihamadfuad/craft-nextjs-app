f69c59324005f74779e2066af955a13d82441b2c
hamadali <ihamadfouad@icloud.com>
2025-07-08 11:30:37 +0300

    add functions for utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,36 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function numberFormatter(num: number, digits?: number) {
+  if (!num) return "0";
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "K" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" },
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  var item = lookup
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return num >= item.value;
+    });
+  return item
+    ? (num / item.value).toFixed(digits || 1).replace(rx, "$1") + item.symbol
+    : "0";
+}
+
+export function capitalize(text: string) {
+  if (!text || typeof text !== "string") return text;
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+export const truncate = (text: string, length: number) => {
+  if (!text || text.length <= length) return text;
+  return `${text.slice(0, length)}...`;
+};
